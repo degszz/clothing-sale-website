80764e02f1e935dda830e60642a04c4bf3447a5e
degsz <125046749+degszz@users.noreply.github.com>
2023-12-19 01:14:38 -0300

    fix(ENV):env redirectUri

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ root.render(
   <Auth0Provider
     domain={domain}
     clientId={clientId}
-    redirect_uri= {window.location.origin || process.env.PUBLIC_URL}
+    redirect_uri= {window.location.origin}
   >
     <App />
   </Auth0Provider>,
